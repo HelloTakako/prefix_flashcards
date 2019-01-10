32aa67fdc23ee61c973108d1ae818720f9eb3e71
Takako <24834291+HelloTakako@users.noreply.github.com>
2019-01-10 02:51:46 -0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -15,23 +15,21 @@ var arr_values = Object.values(arr_question);
 for (val_split in arr_values){
   // arr_val_split is an Array for storing each values
   var arr_val_split = arr_values[val_split].split(',');
-  // console for debugging
-  console.log(arr_val_split);
 }
 
 // Start funtction(the flushcards will start when Shift + Enter are pressed)
 $(window).keydown(function startCards(e){
+  var question_key;
        if(e.shiftKey && e.keyCode == 13) {
 
          // show prefix in quetion area
-         var question_keys = arr_keys[ Math.floor( Math.random() * arr_keys.length ) ] ;
-         $('#question').text(question_keys);
-
+         question_key = arr_keys[ (Math.floor( Math.random() * arr_keys.length ))-1 ] ;
+         $('#question').text(question_key);
          // focus on answer box
          $('#user_input').focus();
+         //return question value
+         console.log(question_key);
        };
-       //return question value
-       return question_keys;
 });
 /* wanna add function to count other cards left and also make same prefix won't appear more than once.*/
 
@@ -39,8 +37,8 @@ $(window).keydown(function startCards(e){
 // submit and compare the answer function
 function submit_answer(){
     var user_input = document.getElementById('user_input').value;
-    var question_keys = startCards();
-    if(question_keys == true) {
+    var question_key = startCards();
+    if(question_key == true) {
         console.log(2);
     }else{
       $('#answer').text("Please input your answer.");
