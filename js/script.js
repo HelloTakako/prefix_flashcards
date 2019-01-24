b99f10868f17e12f10cfb7fde7cd71769043d469
Takako <24834291+HelloTakako@users.noreply.github.com>
2019-01-24 02:22:38 -0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,19 +1,111 @@
 var prefix = {
-    s1 : {
+    p1 : {
       pref : 'a-',
       meaning:'not'
     },
-    s2 : {
+    p2 : {
       pref : 'acro-',
       meaning : 'high'
     },
-    s3 : {
+    p3 : {
       pref : 'allo-',
       meaning : 'other'
     },
-    s4 : {
+    p4 : {
+      pref : 'alter-',
+      meaning : 'at least secondary'
+    },
+    p5 : {
+      pref : 'an-',
+      meaning : 'additional'
+    },
+    p6 : {
+      pref : 'ante-',
+      meaning : 'prior'
+    },
+    p7 : {
+      pref : 'anti-',
+      meaning : 'opposite'
+    },
+    p8 : {
+      pref : 'auto-',
+      meaning : 'by oneself or itself'
+    },
+    p9 : {
+      pref : 'bi-',
+      meaning : 'two'
+    },
+    p10 : {
+      pref : 'co-',
+      meaning : 'together'
+    },
+    p11 : {
       pref : 'contra-',
-      meaning : ['below','against']
+      meaning : ['other','against']
+    },
+    p12 : {
+      pref : 'counter-',
+      meaning : 'against'
+    },
+    p13 : {
+      pref : 'de-',
+      meaning : ['negative','remove']
+    },
+    p14 : {
+      pref : 'di-',
+      meaning : 'two'
+    },
+    p15 : {
+      pref : 'dis-',
+      meaning : ['negative','remove']
+    },
+    p16 : {
+      pref : 'down-',
+      meaning : ['down','reduce']
+    },
+    p17 : {
+      pref : 'dys-',
+      meaning : ['negative','badly','wrongly']
+    },
+    p18 : {
+      pref : 'epi-',
+      meaning : ['upon addition','above']
+    },
+    p19 : {
+      pref : 'extra-',
+      meaning : ['to a greater extent','beyond']
+    },
+    p20 : {
+      pref : 'fore-',
+      meaning : 'before'
+    },
+    p21 : {
+      pref : 'hemi-',
+      meaning : 'half'
+    },
+    p22 : {
+      pref : 'hexa-',
+      meaning : 'six'
+    },
+    p23 : {
+      pref : 'hyper-',
+      meaning : 'beyond'
+    },
+    p24 : {
+      pref : 'ig-',
+      meaning : 'not'
+    },
+    p25 : {
+      pref : 'il-',
+      meaning : ['within','toward','marginal or not']
+    },
+    p26 : {
+      pref : 'im-',
+      meaning : ['within','toward','marginal or not']
+    },
+    p27 : {
+      pref : 'in-',
+      meaning : ['within','toward','marginal or not']
     }
   }
 
@@ -23,12 +115,14 @@ for ( i in prefix ){
   arr_pref.push(i);
 }
 
-// Start function
+// Global variables
 var question_key;
+var next_card = "";
 
+// Start function
 function startCards(){
          // show prefix in quetion area
-         question_key = prefix["s" + (Math.floor(Math.random() * arr_pref.length) + 1)];
+         question_key = prefix["p" + (Math.floor(Math.random() * arr_pref.length) + 1)];
          $('#question').text(question_key.pref);
 
          // focus on answer box
@@ -37,6 +131,7 @@ function startCards(){
          //return question value         
          return question_key;
        };
+
 
 
 // function to compare user's input and answer
@@ -49,6 +144,20 @@ function checkAnswer(){
         if (user_input.value == question_key.meaning ){
           answer.innerHTML = 'Correct! "Ctrl" + "Enter" to next card.' ;
           answer.classList.add('answer_correct');
+
+            // Proceed to next card
+			$(window).keydown(function nextCard(e){
+				  if(answer.classList　== 'answer_correct'){
+			          if(e.ctrlKey && e.keyCode == 13){
+			  	        answer.innerText = "";
+			  	        answer.classList.remove('answer_correct');
+			  	        answer.classList.remove('answer_wrong');
+			          	startCards();
+			          	checkAnswer();
+				      }
+			      }
+			})
+
           if(answer.classList.contains('answer_wrong')){
             answer.classList.remove('answer_wrong');
           }
@@ -59,20 +168,28 @@ function checkAnswer(){
           if(answer.classList.contains('answer_correct')){
             answer.classList.remove('answer_correct');
           }
-        }            
+        }
     }
 }
 
+
+
 // show answer when "show_answer" button is clicked, or Shift + Enter are pressed 
 function showAnswer(){
-
+	var answer = document.getElementById("answer");
+	answer.innerText = question_key.meaning;
 }
+$(window).keydown(function errorAnswer(e){
+          if(e.shiftKey && e.keyCode == 13){
+            showAnswer();
+      }
+})
 
 
 // action when user submitted their answer     
-$(window).keydown(function startCards(e){
+$(window).keydown(function errorAnswer(e){
           if(e.ctrlKey && e.keyCode == 13){
-            var err_txt = 'Please hit \"Shift\" + \"Enter\".';
+            var err_txt = 'Please hit \"Start\" button.';
               if( typeof question_key == "undefined" ){
                  document.getElementById("answer").innerText = err_txt ; 
              } else {
@@ -81,109 +198,11 @@ $(window).keydown(function startCards(e){
       }
 })
 
-// show next card and clear values of user input and answer field
+// show next card and clear values of user input / answer field
          var user_input = document.getElementById('user_input');
          var answer = document.getElementById("answer");
 
-          user_input.value = "";
-          answer.innerText = "";
-
-
-
-
-
-// Start funtction(the flushcards will start when Shift + Enter are pressed)
-// var question_key;
-// $(window).keydown(function startCards(e){
-//        if(e.shiftKey && e.keyCode == 13) {
-
-//          // show prefix in quetion area
-//          question_key = prefix["s" + (Math.floor(Math.random() * arr_pref.length) + 1)];
-//          $('#question').text(question_key.pref);
-
-//          // focus on answer box
-//          $('#user_input').focus();
-//        };
-
-//        //return question value         
-//        return question_key;
-// });
-
-// read suffix.json
-// jQueryでjsonデータを扱ってみる【入門編】
-// $(document).ready(function(){
-//   $.getJSON("js/suffix.json", function(data){
-//     for(var i in data){
-//       $('#question').innerHTML = i;
-//     }
-//   });
-// });
-
-// https://qiita.com/chiiia12/items/9efec29e6b360e3cd38a
-// var httpObj = new XMLHttpRequest();
-// httpObj.open("get", "suffix.json", true);
-//  httpObj.onload = function(){
-//                 var myData = JSON.parse(this.responseText);
-//                 var txt = "";
-//                 for (var i=0; i<myData.length; i++){
-//                    var option = document.createElement("option");
-//                     option.innerText = myData[i].name;
-//                     option.value=myData[i].value;
-//                     document.getElementById("question").appendChild(option); 
-//                 }
-
-//             }
-//        httpObj.send(null);
-
-// // length of 'arr_question' object
-// var arr_length = Object.keys(arr_question).length;
-
-// // keys of 'arr_question' object
-// var arr_keys = Object.keys(arr_question);
-
-// // values of 'arr_question' object
-// var arr_values = Object.values(arr_question);
-
-// // split values
-// for (val_split in arr_values){
-//   // arr_val_split is an Array for storing each values
-//   var arr_val_split = arr_values[val_split].split(',');
-// }
-
-// // Start funtction(the flushcards will start when Shift + Enter are pressed)
-// $(window).keydown(function startCards(e){
-//   var question_key;
-//        if(e.shiftKey && e.keyCode == 13) {
-
-//          // show prefix in quetion area
-//          question_key = arr_keys[ (Math.floor( Math.random() * arr_keys.length ))-1 ] ;
-//          $('#question').text(question_key);
-//          // focus on answer box
-//          $('#user_input').focus();
-//          //return question value
-//          console.log(question_key);
-//        };
-// });
-// /* add function to count other cards left and also make same prefix won't appear more than once.*/
-
-
-// // submit and compare the answer function
-// function submit_answer(){
-//     var user_input = document.getElementById('user_input').value;
-//     var question_key = startCards();
-//     if(question_key == true) {
-//         console.log(2);
-//     }else{
-//       $('#answer').text("Please input your answer.");
-//     }
-//     return false;
-//   };
-
-// //$('#button_submit').on('click',submit_answer);
-// // || $('#button_submit').click()//
-// $(window).keydown(function(e){
-//   if(e.ctrlKey && e.keyCode == 13) {
-//     submit_answer();
-//   }
-
-// });
+	      user_input = "";
+	      answer = "";
+	      user_input.value = "";
+	      answer.innerText = "";
